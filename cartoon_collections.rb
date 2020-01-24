8ed0d77824b195401798c8cff06811aa5d80ec79
Maylene Poulsen <mayleneski@hotmail.com>
2020-01-24 02:04:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def roll_call_dwarves(dwarves)
   
     dwarves.each_with_index do |dwarf, index|
@@ -19,6 +20,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
+  binding.pry
   cheese_types = ["cheddar", "gouda", "camembert"]
   
   array.find do |item|
